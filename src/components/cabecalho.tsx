ff70015f7cb65571ff9703c7d17fcaea1fb53880
Alana Abreu <149194023+N1nh4@users.noreply.github.com>
2025-05-31 12:11:55 -0300

    primeira versao componente cabecalho

--- src/components/cabecalho.tsx
+++ src/components/cabecalho.tsx
@@ -1,7 +1,55 @@
-export default function Cabecalho() {
+import Link from "next/link";
+
+
+interface NavLink {
+  id: number;
+  href: string;
+  label: string;
+}
+
+
+interface CabecalhoProps {
+  navLinks: NavLink[];
+}
+
+
+export default function Cabecalho({ navLinks }: CabecalhoProps) {
+  const shadowStyle = {
+    boxShadow: "4px 8px 15px rgba(0, 0, 0, 0.25)",
+  };
+
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1> TESTE </h1>
-    </div>
+    <header
+      className="w-full h-24 bg-gradient-to-r from-[#004E4C] to-verdeClaro"
+      style={shadowStyle} // Aplica a sombra aqui
+    >
+      <div className="h-full flex items-center px-14 justify-between">
+        {/* Logo */}
+        <div className="flex items-center">
+          <Link
+            href="/inicial"
+          >
+            <span className="text-3xl font-bold text-white">UpaAgora</span>
+          </Link>
+        </div>
+
+
+        {/* Navegação */}
+        <nav className="flex items-center space-x-14 text-3xl">
+          {navLinks.map((link, id) => (
+            <Link
+              href={link.href}
+              key={id}
+              className="text-white hover:text-gray-300 transition-colors"
+            >
+              {link.label}
+            </Link>
+          ))}
+        </nav>
+      </div>
+    </header>
+
+
   );
 }
