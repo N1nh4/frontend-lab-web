44b70e5616fc35079a5563fa3cba556ab989f971
Alana Abreu <149194023+N1nh4@users.noreply.github.com>
2025-05-31 14:58:00 -0300

    adicionando o carrosel

--- src/app/(paginas)/(public)/inicial/page.tsx
+++ src/app/(paginas)/(public)/inicial/page.tsx
@@ -1,7 +1,111 @@
+import Cabecalho from "@/components/cabecalho";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
 export default function Inicial() {
+  const navLinks = [
+    { id: 1, label: 'Registrar lotação', href: '/registrar-lotacao' },
+    { id: 2, label: 'Ir para o mapa', href: '/mapa' },
+    { id: 3, label: 'Ranking', href: '/ranking' },
+    { id: 4, label: 'Entrar', href: '/entrar' },
+    { id: 5, label: 'Sobre nos', href: '/sobre-nos' },
+    { id: 6, label: 'Criar conta', href: '/criar-conta' },
+  ];
+
+  const itemHorizontalSpacing = "20"; 
+
+  const cardWidth = "w-[1040px]";
+  const cardHeight = "h-[300px]"; 
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1> TESTE </h1>
+    <div className="w-full min-h-screen">
+      <Cabecalho navLinks={navLinks} />
+      <main className="w-full py-20 bg-gradient-to-r from-[#004E4C] to-verdeClaro">
+        <Carousel
+          opts={{
+            align: "center", 
+            loop: true,
+          }}
+          className="w-full mx-auto" 
+        >
+          <CarouselContent className={`-ml-${itemHorizontalSpacing}`}>
+            <CarouselItem className={`pl-${itemHorizontalSpacing} basis-auto`}>
+              <div className={`p-10 bg-white rounded-2xl shadow-sm ${cardWidth} ${cardHeight} flex flex-col justify-between relative`}>
+                <div className="flex flex-row items-center justify-between">
+                  <div className="flex flex-col w-4/6">
+                    <h1 className="text-5xl font-semibold text-gray-800 mb-10">
+                      Atualize o status da sua unidade de saúde
+                    </h1>
+                    <p className="text-3xl text-gray-600 mb-4">
+                      Ajude outros usuários informando a situação atual das emergências.
+                    </p>
+                  </div>
+                </div>
+                <Button
+                  className="bg-[#106A43] hover:bg-[#0c5033] text-white text-lg rounded-lg w-4/12 flex items-center justify-center absolute bottom-10 right-10"
+                >
+                  REGISTRAR LOTAÇÃO
+                </Button>
+              </div>
+            </CarouselItem>
+
+
+            <CarouselItem className={`pl-${itemHorizontalSpacing} basis-auto`}>
+              <div className={`p-6 bg-white rounded-2xl shadow-sm ${cardWidth} ${cardHeight} flex flex-col justify-between`}>
+                <div>
+                  <h1 className="text-2xl font-semibold text-gray-800 mb-2">Seu impacto conta</h1>
+                  <p className="text-base text-gray-600 mb-4">Veja como suas contribuições ajudam a comunidade a se manter informada.</p>
+                </div>
+                <Button className="bg-[#106A43] hover:bg-[#0c5033] text-white">
+                  VER MEUS PONTOS
+                </Button>
+              </div>
+            </CarouselItem>
+
+            <CarouselItem className={`pl-${itemHorizontalSpacing} basis-auto`}>
+              <div
+                className={`p-10 bg-[#106A43] rounded-2xl shadow-sm text-white ${cardWidth} ${cardHeight} flex flex-row justify-between items-center`}
+              >
+                <div className="flex flex-col w-1/2 ">
+                  <h1 className="text-5xl font-semibold mb-6">Objetivo 3: Saúde e Bem-Estar</h1>
+                  <p className="text-2xl mb-4 ">Contribuindo para os Objetivos de Desenvolvimento Sustentável.</p>
+                </div>
+                <div className="flex justify-end w-1/2">
+                  <Image
+                    src="/images/ods3.png"
+                    alt="ODS 3: Saúde e Bem-Estar"
+                    width={450}
+                    height={450}
+                    
+                  />
+                </div>
+              </div>
+            </CarouselItem>
+
+
+            <CarouselItem className={`pl-${itemHorizontalSpacing} basis-auto`}>
+              <div className={`p-6 bg-white rounded-2xl shadow-sm ${cardWidth} ${cardHeight} flex flex-col justify-between`}>
+                <div>
+                  <h1 className="text-2xl font-semibold text-gray-800 mb-2">Descubra unidades próximas</h1>
+                  <p className="text-base text-gray-600 mb-4">Utilize o mapa para encontrar os hospitais e clínicas mais próximos de você.</p>
+                </div>
+                <Button className="bg-[#106A43] hover:bg-[#0c5033] text-white">
+                  IR PARA O MAPA
+                </Button>
+              </div>
+            </CarouselItem>
+          </CarouselContent>
+          <CarouselPrevious className="absolute left-4 top-1/2 -translate-y-1/2" />
+          <CarouselNext className="absolute right-4 top-1/2 -translate-y-1/2" />
+        </Carousel>
+      </main>
     </div>
   );
 }
--- src/components/cabecalho.tsx
+++ src/components/cabecalho.tsx
@@ -1,28 +1,24 @@
 import Link from "next/link";
 
-
 interface NavLink {
   id: number;
   href: string;
   label: string;
 }
 
-
 interface CabecalhoProps {
   navLinks: NavLink[];
 }
-
 
 export default function Cabecalho({ navLinks }: CabecalhoProps) {
   const shadowStyle = {
     boxShadow: "4px 8px 15px rgba(0, 0, 0, 0.25)",
   };
 
-
   return (
     <header
-      className="w-full h-24 bg-gradient-to-r from-[#004E4C] to-verdeClaro"
-      style={shadowStyle} // Aplica a sombra aqui
+      className="w-full h-24 bg-gradient-to-r from-[#004E4C] to-verdeClaro relative z-10" 
+      style={shadowStyle}
     >
       <div className="h-full flex items-center px-14 justify-between">
         {/* Logo */}
@@ -33,7 +29,6 @@ export default function Cabecalho({ navLinks }: CabecalhoProps) {
             <span className="text-3xl font-bold text-white">UpaAgora</span>
           </Link>
         </div>
-
 
         {/* Navegação */}
         <nav className="flex items-center space-x-14 text-3xl">
@@ -49,7 +44,5 @@ export default function Cabecalho({ navLinks }: CabecalhoProps) {
         </nav>
       </div>
     </header>
-
-
   );
 }
