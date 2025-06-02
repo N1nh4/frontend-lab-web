1aacaff317689addd7179bc072206862357c91e1
Hugo <vieirahg@gmail.com>
2025-06-02 10:53:33 -0300

    testando

--- src/app/(paginas)/(public)/perfil/page.tsx
+++ src/app/(paginas)/(public)/perfil/page.tsx
@@ -1,8 +1,11 @@
 export default function Perfil() {
   return (
-    <div className="h-screen w-screen bg-amber-800">
-      <h1> OLA MUNDO </h1> 
-      <h1> TESTEEEE</h1>
+    <div className="h-screen w-screen bg-green-800">
+      <h1> GERENCIAMENTO DE CONTA </h1> 
+      <h1> TESTE</h1>
+      
     </div>
+  
+   
   );
 }
