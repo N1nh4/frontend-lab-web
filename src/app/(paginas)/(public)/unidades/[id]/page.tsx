31abeb9d797d580c75d301eaaf3df630c2eab9fc
Alana Abreu <149194023+N1nh4@users.noreply.github.com>
2025-06-08 14:23:01 -0300

    alert dialog add

--- src/app/(paginas)/(public)/unidades/[id]/page.tsx
+++ src/app/(paginas)/(public)/unidades/[id]/page.tsx
@@ -7,6 +7,8 @@ import React from "react";
 import Image from "next/image";
 import { UnidadeSaude, allUnidadesData } from '@/data/unidades'; 
 import { renderStars, renderUserIcons } from "@/lib/utils/rendering";
+import { Info, User } from "lucide-react";
+import { AlertDialog, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogTitle, AlertDialogTrigger, AlertDialogFooter, AlertDialogHeader } from "@/components/ui/alert-dialog";
 
 export default function UnidadeDetalhesPage() {
     const navLinks = [
@@ -52,17 +54,53 @@ export default function UnidadeDetalhesPage() {
                 </div>
                 <div className="flex flex-col justify-center text-lg  w-3/5 ">
                     <h2 className="text-2xl font-bold  mb-4">{unidade.titulo}</h2>
-                    <div className="flex items-center mb-4">
+                    <div 
+                        className="flex items-center mb-4"
+                        onClick={() => alert('Avaliação clicada!')} // Exemplo de ação ao clicar
+                    >
                         {renderStars(unidade.avaliacaoEstrela, 24)}
+                        <span className="ml-2 text-xs text-gray-600">({unidade.avaliacaoEstrela})</span>
+                        
                     </div>
                     <p className="mb-2"><span className="font-bold">Endereço:</span> {unidade.endereco}</p>
                     <p className="mb-2"><span className="font-bold">Telefone:</span> {unidade.telefone}</p>
-                    <p className="mb-2 font-bold">Status: {unidade.status}</p>
+                    <p className="flex mb-2 font-bold items-center">
+                        Status: {unidade.status}
+                    <AlertDialog >
+                        <AlertDialogTrigger asChild>
+                            <span className="cursor-pointer">
+                                <Info size={12} color="black" className="ml-2 "/>
+                            </span>
+                        </AlertDialogTrigger>
+                        <AlertDialogContent>
+                            <AlertDialogHeader>
+                                <AlertDialogTitle>Como avaliar o Status de lotação?</AlertDialogTitle>
+                                <AlertDialogDescription>
+                                     
+                                    <ul className="pl-5">
+                                        <li className="flex items-center"><span className="flex items-center gap-1 font-bold text-emerald-500"> <User /> VAZIO</span>: O local está quase sem ninguém.</li>
+                                        <li className="flex items-center"><span className="flex items-center gap-1 font-bold text-emerald-500"> <User /> POUCO VAZIO</span>: O local está com pouca gente.</li>
+                                        <li className="flex items-center"><span className="flex items-center gap-1 font-bold text-amber-500"> <User /> MODERADO</span>: O local está com uma quantidade moderada de pessoas.</li>
+                                        <li className="flex items-center"><span className="flex items-center gap-1 font-bold text-red-500"> <User /> CHEIO</span>: O local está cheio.</li>
+                                        <li className="flex items-center"><span className="flex items-center gap-1 font-bold text-red-500"> <User /> MUITO CHEIO</span>: O local está muito cheio.</li>
+                                    </ul>
+                                </AlertDialogDescription>
+                            </AlertDialogHeader>
+                            <AlertDialogFooter>
+                                <AlertDialogCancel className="bg-gray-200 hover:bg-gray-300">Fechar</AlertDialogCancel>
+                            </AlertDialogFooter>
+                        </AlertDialogContent>
+                    </AlertDialog>
+                    </p>
                   
                     <span className="flex">{renderUserIcons(unidade.status, 32)}</span>
                     <p className="italic text-xs"> {unidade.ultimaAtualizacao}</p>
                     
+                    <button className="mt-4 h-10 font-bold bg-verdeEscuro text-white rounded-lg">RESGISTRAR LOTAÇÃO</button>
                 </div>
+            </div>
+            <div>
+                
             </div>
        </main>
     );
