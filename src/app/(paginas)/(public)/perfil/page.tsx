7f43bef242494ebf3197a68303eeb20a07b00d1a
Hugo <vieirahg@gmail.com>
2025-06-19 17:42:56 -0300

    finalizado sem tá responsivo

--- src/app/(paginas)/(public)/perfil/page.tsx
+++ src/app/(paginas)/(public)/perfil/page.tsx
@@ -21,28 +21,93 @@ export default function Perfil() {
       <Cabecalho navLinks={navLinks} />
       <div className ="w-full flex">
        
-          <div className="flex flex-col items-start justify-center w-1/3 h-full z-20 pl-30 mt-30">
-            <div className=" z-9  text-white">
+          <div className="flex flex-col items-start justify-center w-1/3 h-full z-20 pl-30 mt-25">
+            <div className="mt-15 fixed z-9  text-white">
               <h1 className = "font-bold text-[200%]">GERENCIAMENTO</h1>
               <h1 className = "font-bold text-[200%] mt-[-12px]">DE CONTA</h1>
             </div>
 
-            <div className="flex z-9 text-black mt-2 w-80">
+            <div className="fixed flex z-9 text-black mt-55 w-80">
               <p className="text-xl leading-none">Preencha ou edite os campos abaixo e salve as alterações para manter seu perfil atualizado.</p>
             </div>
 
             
-            <div className="flex flex-col items-start justify-center z-20 ml-18 mt-5">
+            <div className="fixed flex flex-col items-start justify-center z-20 ml-18 mt-110">
               <UserRound className="relative rounded-full bg-blue-100 cursor-pointer"size={110} strokeWidth={1}/>
+              <div className="relative ml-[62%] mt-[-25%] w-10 h-10 bg-amber-50 z-20 rounded-full flex items-center justify-center cursor-pointer">
+                <Camera className="text-emerald-800 z-30"size={30} strokeWidth={3.5} />    
+              </div> 
             </div>
                 
-            <div className="relative ml-[42%] mt-[-11%] w-10 h-10 bg-amber-50 z-20 rounded-full flex items-center justify-center cursor-pointer">
-              <Camera className="text-emerald-800 z-30"size={30} strokeWidth={3.5} />    
-            </div> 
+            
           </div>
 
           <div className ="flex flex-col items-start justify-center">
-            <div className="w-220 h-59  mt-15 rounded-xl overflow-hidden flex">
+            <div className="w-220 h-59  mt-15 rounded-xl overflow-hidden flex"> 
+              <div className="w-3/8 bg-gray-200">
+                <div className=" z-9  text-emerald-800 ml-5 mt-10 w-46">
+                  <h1 className = "font-bold text-[200%] leading-none">Informações Pessoais</h1>
+                  
+                </div>
+                <div className="flex z-9 text-black ml-5 mt-5 w-86">
+                  <p className="leading-none">Essa informação é particular e não será compartilhada com outras pessoas.</p>
+                </div>
+              </div>
+
+              <div className="w-5/8 bg-gray-50">{/* ESSA PORRA AQUI ENGLOBA TUDO */}
+                <div className ="flex flex-col items-start justify-center">
+                  <div className="w-55 h-12 ml-15 mt-[7%] rounded-xl overflow-hidden flex">
+                    <div className="w-1/1 bg-gray-200">
+                      <div className=" z-9  text-gray-400 ml-2 mt-1 w-46 ">
+                        <h1 className = "font-bold text-[70%] leading-none">NOME:</h1>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="w-39.5 h-12 ml-15 mt-[5.5%] rounded-xl overflow-hidden flex">
+                    <div className="w-1/1 bg-gray-200">
+                      <div className=" z-9  text-gray-400 ml-2 mt-1 w-46">
+                        <h1 className = "font-bold text-[70%] leading-none">DATA DE NASCIMENTO:</h1>
+                        
+                      </div>
+                    </div>
+                  </div>
+
+                  
+                </div>
+              </div>{/*FINAL DESSA PORRA AQUI ENGLOBA TUDO */}
+              <div className="w-5/8 bg-gray-50">{/* ESSA PORRA AQUI ENGLOBA TUDO */}
+                <div className ="flex flex-col items-start justify-center">
+                  <div className="w-55 h-12 ml-[5%] mt-[6.55%] rounded-xl overflow-hidden flex">
+                    <div className="w-1/1 bg-gray-200">
+                      <div className=" z-9  text-gray-400 ml-2 mt-1 w-46 ">
+                        <h1 className = "font-bold text-[70%] leading-none">CIDADE/ESTADO:</h1>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="w-70 h-12 ml-[-17%] mt-[6%] rounded-xl overflow-hidden flex">
+                    <div className="w-1/1 bg-gray-200">
+                      <div className=" z-9  text-gray-400 ml-2 mt-1 w-46 ">
+                        <h1 className = "font-bold text-[70%] leading-none">ENDEREO DE EMAIL:</h1>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="w-40 h-8 ml-[24%] mt-[15%] rounded-xl overflow-hidden flex">
+                    <div className="w-1/1 bg-emerald-700 cursor-pointer">
+                      <div className=" z-9  text-white ml-[14.5%] mt-[6%]">
+                        <h1 className = "font-bold text-[70%] leading-none">SALVAR ALTERAÇÕES</h1>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            
+
+            
+            <div className="w-220 h-59 mt-15 mb-10 rounded-xl overflow-hidden flex">
+              <div className="w-220 h-59 rounded-xl overflow-hidden flex">
               <div className="w-3/8 bg-gray-200">
                 <div className=" z-9  text-emerald-800 ml-5 mt-10 w-46">
                   <h1 className = "font-bold text-[200%] leading-none">Informações Complementares</h1>
@@ -53,7 +118,7 @@ export default function Perfil() {
                 </div>
               </div>
 
-              <div className="w-5/8 bg-gray-50">{/* ESSA PORRA AQUI ENGLOBA TUDO */}
+              <div className="w-5/8 bg-gray-50">
                 <div className ="flex flex-col items-start justify-center">
                   <div className="w-55 h-12 ml-15 mt-[7%] rounded-xl overflow-hidden flex">
                     <div className="w-1/1 bg-gray-200">
@@ -74,8 +139,8 @@ export default function Perfil() {
 
                   
                 </div>
-              </div>{/*FINAL DESSA PORRA AQUI ENGLOBA TUDO */}
-              <div className="w-5/8 bg-gray-50">{/* ESSA PORRA AQUI ENGLOBA TUDO */}
+              </div>
+              <div className="w-5/8 bg-gray-50">
                 <div className ="flex flex-col items-start justify-center">
                   <div className="w-55 h-12 ml-[5%] mt-[6%] rounded-xl overflow-hidden flex">
                     <div className="w-1/1 bg-gray-200">
@@ -102,36 +167,68 @@ export default function Perfil() {
                 </div>
               </div>
             </div>
-            
-
-            <div className="w-220 h-59  mt-15 rounded-xl overflow-hidden flex">
-              <div className="w-3/8 bg-gray-200">
-                <div className=" z-9  text-emerald-800 ml-5 mt-10 w-46">
-                  <h1 className = "font-bold text-[200%] leading-none">Segurança e privadicade</h1>
-                  
-                </div>
-                <div className="flex z-9 text-black ml-5 mt-5 w-86">
-                  <p className="leading-none">Essa informação é particular e não será compartilhada com outras pessoas.</p>
-                </div>
-              </div>
-              <div className="w-5/8 bg-gray-50">{/* ESSA PORRA AQUI ENGLOBA TUDO */}</div>
               
-            </div>
-            <div className="w-220 h-59  mt-15 rounded-xl overflow-hidden flex">
-              <div className="w-3/8 bg-gray-200">
-                <div className=" z-9  text-emerald-800 ml-5 mt-10 w-46">
-                  <h1 className = "font-bold text-[200%] leading-none">Segurança e privadicade</h1>
-                  
-                </div>
-                <div className="flex z-9 text-black ml-5 mt-5 w-86">
-                  <p className="leading-none">Essa informação é particular e não será compartilhada com outras pessoas.</p>
-                </div>
-              </div>
-              <div className="w-5/8 bg-gray-50">{/* ESSA PORRA AQUI ENGLOBA TUDO */}</div>
               
-            </div>
-
           </div>
+          <div className="w-220 h-59 mb-10 rounded-xl overflow-hidden flex"> {/* aq fica o começo do quadro */}
+              <div className="w-3/8 bg-gray-200">
+                <div className=" z-9  text-emerald-800 ml-5 mt-10 w-46">
+                  <h1 className = "font-bold text-[200%] leading-none">Segurança e privadicade</h1>
+                  
+                </div>
+                <div className="flex z-9 text-black ml-5 mt-5 w-86">
+                  <p className="leading-none">Essa informação é particular e não será compartilhada com outras pessoas.</p>
+                </div>
+              </div>
+
+              <div className="w-4/8 bg-gray-50">{/* ESSA PORRA AQUI ENGLOBA TUDO */}
+                <div className ="flex flex-col items-start justify-center">
+                  <div className="w-55 h-12 ml-15 mt-[7%] rounded-xl overflow-hidden flex">
+                    <div className="w-1/1 bg-gray-200">
+                      <div className=" z-9  text-gray-400 ml-2 mt-1 w-46 ">
+                        <h1 className = "font-bold text-[70%] leading-none">ID:</h1>
+                      </div>
+                    </div>
+                  </div>
+                <div className="w-55 h-12 ml-15 mt-[7%] rounded-xl overflow-hidden flex">
+                  <div className="w-1/1 bg-gray-200">
+                    <div className=" z-9  text-gray-400 ml-2 mt-1 w-46 ">
+                      <h1 className = "font-bold text-[70%] leading-none">NOVA SENHA:</h1>
+                    </div>
+                  </div>
+                </div>
+
+                  
+                </div>
+              </div>{/*FINAL DESSA PORRA AQUI ENGLOBA TUDO */}
+              <div className="w-5/8 bg-gray-50">{/* ESSA PORRA AQUI ENGLOBA TUDO */}
+                <div className ="flex flex-col items-start justify-center">
+                <div className="w-55 h-12 ml-4.5 mt-[7.2%]  rounded-xl overflow-hidden flex">
+                  <div className="w-1/1 bg-gray-200">
+                    <div className=" z-9  text-gray-400 ml-2 mt-1 w-46 ">
+                      <h1 className = "font-bold text-[70%] leading-none">SENHA ATUAL:</h1>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="w-55 h-12 ml-4.5 mt-[7.2%]  rounded-xl overflow-hidden flex">
+                  <div className="w-1/1 bg-gray-200">
+                    <div className=" z-9  text-gray-400 ml-2 mt-1 w-46 ">
+                      <h1 className = "font-bold text-[70%] leading-none">CONFIRMAR SENHA:</h1>
+                    </div>
+                  </div>
+                </div>
+                  <div className="w-40 h-8 ml-[29%] mt-[16%] rounded-xl overflow-hidden flex">
+                    <div className="w-1/1 bg-emerald-700 cursor-pointer">
+                      <div className=" z-9  text-white ml-[14.5%] mt-[6%]">
+                        <h1 className = "font-bold text-[70%] leading-none">SALVAR ALTERAÇÕES</h1>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
           
 
           
